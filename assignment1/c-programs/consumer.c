39e01489671cbbc28fb59673a65c056f28f98143
Amber <233861108+Oddwyer@users.noreply.github.com>
2026-01-26 21:38:32 -0500

    Debugging signals

--- assignment1/c-programs/consumer.c
+++ assignment1/c-programs/consumer.c
@@ -6,16 +6,14 @@
 #include <unistd.h> // getopt, read, write, close
 
 // variable triggered during signal event intialized to 0.
- volatile sig_atomic_t shutdown_flag = 0;
+volatile sig_atomic_t shutdown_flag = 0;
+volatile sig_atomic_t stats_flag = 0;
 
 // signal handler 1 (Ctrl + C)
- void handle_sigint(int sig) { shutdown_flag = 1; }
+void handle_sigint(int sig) { shutdown_flag = 1; }
 
-/*signal handler 2 (SIGUSR1)
-void handle_sigusr1(int sig) {
-infinite_flag = 1;
-}*/
-
+// signal handler 2 (SIGUSR1)
+void handle_sigusr1(int sig) { stats_flag = 1; }
 
 int main(int argc, char **argv) {
 
@@ -30,40 +28,40 @@ int main(int argc, char **argv) {
   sa.sa_flags = 0;
   sigaction(SIGINT, &sa, NULL);
 
-  /*struct sigaction sa1;
-  sa1.sa_handler = SIG_IGN;
+  struct sigaction sa1;
+  sa1.sa_handler = handle_sigusr1;
   sigemptyset(&sa1.sa_mask);
   sa1.sa_flags = 0;
-  sigaction(SIGUSR1, &sa1, NULL);*/
+  sigaction(SIGUSR1, &sa1, NULL);
+
+  // GETOPT() Method: required to parse CLI flags when reading in from a file
+  // vs stdin.
+  // 1. needed getopt() variables
+  int opt = 0;
+  int maxlines = 0;
+  int linecount = 0;
+  int charcount = 0;
+  bool maxpassed = false;
+  bool verbose = false;
+
+  // 2.getopt() method
+  while ((opt = getopt(argc, argv, "vn:")) !=
+         -1) { // note: ":" removed after v if only flag and no argument passed.
+    switch (opt) {
+    case 'v':
+      verbose = true;
+      break;
+    case 'n':
+      maxlines = atoi(optarg); // atio = "ASCII to int"
+      maxpassed = true;
+      break;
+    default:
+      fprintf(stderr, "Usage: %s [-v]  [-n max lines]\n", argv[0]);
+      exit(1);
+    }
+  }
 
   while (!shutdown_flag) {
-    // GETOPT() Method: required to parse CLI flags when reading in from a file
-    // vs stdin.
-    // 1. needed getopt() variables
-    int opt = 0;
-    int maxlines = 0;
-    int linecount = 0;
-    int charcount = 0;
-    bool maxpassed = false;
-    bool verbose = false;
-
-    // getopt() method
-    while (
-        (opt = getopt(argc, argv, "vn:")) !=
-        -1) { // note: ":" removed after v if only flag and no argument passed.
-      switch (opt) {
-      case 'v':
-        verbose = true;
-        break;
-      case 'n':
-        maxlines = atoi(optarg); // atio = "ASCII to int"
-        maxpassed = true;
-        break;
-      default:
-        fprintf(stderr, "Usage: %s [-v]  [-n max lines]\n", argv[0]);
-        exit(1);
-      }
-    }
 
     // reading file from stdin -> does not require f(open)!
     // fgets(storage location, size of storage, input file)
@@ -82,17 +80,23 @@ int main(int argc, char **argv) {
       }
       // count characters
       charcount += strlen(readfile);
+
+      // SIGUSR1 flag condition
+      if (stats_flag) {
+        printf("Current line count: %d\nCurrent character count: %d\n", linecount, charcount);
+        stats_flag = 0;
+      }
     }
 
     // if one line of text only (thus no \n), count line
     if (charcount > 0 && linecount == 0) {
       linecount++;
     }
-
+}
     // print stats to standard error
     fprintf(stderr, "Line count: %d\nCharacter count: %d\n", linecount,
             charcount);
-  }
+  
 
   printf("Shutting down...");
 
--- assignment1/c-programs/producer.c
+++ assignment1/c-programs/producer.c
@@ -7,7 +7,9 @@
 
 volatile sig_atomic_t shutdown_flag = 0;
 
-void handle_sigint(int sig) { shutdown_flag = 1; }
+void handle_sigint(int sig) {
+ shutdown_flag = 1;
+ }
 
 int main(int argc, char **argv) {
 
@@ -21,47 +23,49 @@ int main(int argc, char **argv) {
   sa.sa_flags = 0;
   sigaction(SIGINT, &sa, NULL);
 
+  // GETOPT() Method: required to parse CLI flags  when reading in from a file
+  // vs stdin.
+  // 1. getopt() variables
+  int opt = 0;
+  int buffer_size = 4096; // 4096 by default
+  char *filename = NULL;  // no file by default
 
-  // run whie not trigger by event signal
-  while (!shutdown_flag) {
-
-    // GETOPT() Method: required to parse CLI flags  when reading in from a file
-    // vs stdin.
-    // 1. getopt() variables
-    int opt = 0;
-    int buffer_size = 4096; // 4096 by default
-    char *filename = NULL;  // no file by default
-
-    // 2. getopt() method
-    while ((opt = getopt(argc, argv, "f:b:")) != -1) {
-      switch (opt) {
-      case 'f':
-        filename = optarg; // stores user input file name from CLI
-        // point to new opened file stream
-        pfile = fopen(filename, "r");
-        break;
-      case 'b':
-        buffer_size = atoi(optarg); // stores user input buffer size from CLI
-        break;
-      default:
-        fprintf(stderr, "Usage: %s [-f file] [-b size]\n", argv[0]);
-        exit(1);
-      }
+  // 2. getopt() method
+  while ((opt = getopt(argc, argv, "f:b:")) != -1) {
+    switch (opt) {
+    case 'f':
+      filename = optarg; // stores user input file name from CLI
+      // point to new opened file stream
+      pfile = fopen(filename, "r");
+      break;
+    case 'b':
+      buffer_size = atoi(optarg); // stores user input buffer size from CLI
+      break;
+    default:
+      fprintf(stderr, "Usage: %s [-f file] [-b size]\n", argv[0]);
+      exit(1);
     }
+  }
 
-    char readfile[buffer_size]; // create storage array for streamed characters
-                                // IF file exists
+  char readfile[buffer_size]; // create storage array for streamed characters
+                              // IF file exists
+
+  // run while not trigger by event signal
+  while (!shutdown_flag) {
 
     // NOTE: reading file from stdn -> does not require f(open)!
     // fgets(storage location, size of storage, input file)
+
     while (fgets(readfile, sizeof(readfile), pfile) != NULL) {
 
       // printf prints to stdout by default
       printf("%s", readfile);
     }
+  // solution for improper line count ensures all data passed to consumer.
+  fflush(stdout);
+
   }
 
-  printf("Exiting program...\n");
 
   // close file
   fclose(pfile);
